10c8ca746b5bc09970baac3ee0ccb6fcb2556395
Élisson Michael <elissonmichael@yahoo.com.br>
2019-05-15 15:59:02 -0300

    Exercise 4 | Refactor for Solution Optimization

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,5 @@
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
 export function value(colors) {
-  let code = ''
-  for (let color of colors) code += COLORS.indexOf(color);
-  return Number(code);
+  return Number(colors.map(color => COLORS.indexOf(color)).join(''));
 }
